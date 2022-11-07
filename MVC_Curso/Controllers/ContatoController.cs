bbab6662883db78f6cf5972b063946ec593029e3
Vinicius Fraga <viniciusfrg@outlook.com.br>
2022-11-06 23:44:18 -0300

    Criado TagHelper para enviado de emails

---
+++ MVC_Curso/Controllers/ContatoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Curso.Controllers {
+    public class ContatoController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
