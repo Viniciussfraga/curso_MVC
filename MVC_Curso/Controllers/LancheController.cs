4f9f73889a7b9146897b9c88939dcbe3d50bcced
Vinicius Fraga <viniciusfrg@outlook.com.br>
2022-11-07 16:09:10 -0300

    Incluso link que mostra os detalhes quando clica no nome do lanche, botões para adicionar no carrinho e continuar comprando

--- MVC_Curso/Controllers/LancheController.cs
+++ MVC_Curso/Controllers/LancheController.cs
@@ -18,28 +18,29 @@ namespace MVC_Curso.Controllers {
             string categoriaAtual = string.Empty;
 
             if (string.IsNullOrEmpty(categoria)) {
-                lanches = _lancheRepository.Lanches.OrderBy(l=> l.LancheId);
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
                 categoriaAtual = "Todos os lanches";
-            }
-            else if(string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase)){
-                lanches = _lancheRepository.Lanches
-                    .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
-                    .OrderBy(l => l.Nome);
             }
             else {
                 lanches = _lancheRepository.Lanches
-                    .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
-                    .OrderBy(l => l.Nome);
+                        .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
+                        .OrderBy(c => c.Nome);
+
             }
             categoriaAtual = categoria;
 
             var lanchesListViewModel = new LancheListViewModel {
                 Lanches = lanches,
-                CategoriaAtual=categoriaAtual,
+                CategoriaAtual = categoriaAtual,
 
             };
             return View(lanchesListViewModel);
 
-           }
+        }
+
+        public IActionResult Details(int lancheId) {
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
+        }
     }
 }
